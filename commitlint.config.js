0d1d6100ad889f1ed49942ddf56cdbb5f1699384
Alan North <alanscodelog@gmail.com>
2025-04-13 15:38:25 -0300

    feat: dropped casing rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -12,11 +12,8 @@ export default {
 		// The rest pretty much match @commitlint/config-conventional, but I didn't want to add another dependency, also having all the rules in one place makes things clearer.
 		'body-leading-blank': [ 2, 'always' ],
 		'footer-leading-blank': [ 2, 'always' ],
-		'scope-case': [ 2, 'always', 'lower-case' ],
-		'subject-case': [ 2, 'never', [ 'sentence-case', 'start-case', 'pascal-case', 'upper-case' ] ],
 		'subject-empty': [ 2, 'never' ],
 		"subject-full-stop": [ 2, "never" ],
-		'type-case': [ 2, 'always', 'lower-case' ],
 		'type-empty': [ 2, 'never' ],
 	}
 }
